f523bdc7d18bd66cd36b7b4871ba081f8bc619e0
ltorrico <linet.torrico@dharbor.com>
2018-10-04 15:21:43 -0400

    Basic project
    Basic project

---
+++ src/main/java/com/therelinettorrico/demo/TasksApplication.java
@@ -0,0 +1,14 @@
+package com.therelinettorrico.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("hola mundo");
+        SpringApplication.run(TasksApplication.class, args);
+    }
+}
